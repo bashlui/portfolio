324175d7da5e7d3166180aaa9ad041bd2bda4726
bashlui <luisboltech005@outlook.com>
2024-01-27 12:22:21 -0600

    List added

--- components/Info.js
+++ components/Info.js
@@ -1,15 +1,19 @@
 export const Info = () => {
     return (
         <div className="bg-gray-700 rounded-md border-8 border-y-gray-500 animate-fade-in-left">
-            <h1 className="text-gray-50 py-3 px-4 text-center">About me</h1>
+            <h1 className="text-gray-50 py-3 px-4 text-center m-4">"Create a path to the future"</h1>
             <div className="columns-3">
                 <div className="">
                     <h2 className="text-gray-50 py-3 px-4 text-center">Who is Luis?</h2>
-                    <p className="text-gray-50 text-left mx-4 my-4">B.S. in Computer Science student at ITESM. I'm from México</p>
+                    <p className="text-gray-50 text-left mx-4 my-4">I'm a full time student at ITESM. I love technologies and programming!</p>
                     <h2 className="text-gray-50 py-3 px-4 text-center">Skills</h2>
-                    <p className="text-gray-50 text-left mx-4 my-4">B.S. in Computer Science student at ITESM. I'm from México</p>
+                    <ul className="text-gray-50 text-left mx-4 my-4">
+                        <li className="list-disc ">Great team work and communication</li>
+                        <li className="list-disc ">Problem solving</li>
+                        <li className="list-disc ">Making ideas and deploy them it's my thing</li>
+                    </ul>
                     <h2 className="text-gray-50 py-3 px-4 text-center">My mission</h2>
-                    <p className="text-gray-50 text-left mx-4 my-4">Bring solutions to any problem and </p>
+                    <p className="text-gray-50 text-left mx-4 my-4">Bring solutions to any sort of problem, develop more skills in my path and bring the best product to my clients.</p>
                 </div>
             </div>
         </div>
