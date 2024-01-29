d1c45a012953a2c36a2c5a3a301eed138cecbede
bashlui <luisboltech005@outlook.com>
2024-01-29 12:08:22 -0600

    Fix: animation needs to fade

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,17 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Urbanist'],
       },
       keyframes: {
+        'desplazamiento-infinito': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(40%)' },
+        },
         'fade-in-left': {
           '0%': {
             opacity: '0',
@@ -28,8 +27,10 @@ module.exports = {
         }
       },
       animation: {
+        'animate-scrolling-svg': 'desplazamiento-infinito 5s linear infinite',
         'fade-in-left': 'fade-in-left 1s ease-out'
       }
     },
   },
+  plugins: [],
 }
