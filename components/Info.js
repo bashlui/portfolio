e34e05c98e74e54f57156dc540f39c32667d2f15
bashlui <luisboltech005@outlook.com>
2024-02-06 19:17:21 -0600

    más info

--- components/Info.js
+++ components/Info.js
@@ -3,16 +3,16 @@ export const Info = () => {
         <div className="bg-stone-600 px-4 py-16 h-screen">
             <h1 className="text-gray-50 py-8 text-center m-5 bg-stone-500">"Create a path to the future".</h1>
             <div className="text-gray-50 bg-stone-500 flex items-center py-8 m-5">
-                <h2 className="text-left m-5">Helping is important to me!</h2>
-                <p className="flex-auto px-4 text-center">Working in teams and deploy projects for a purpose it's important to me. I'm compromised to help and develop skills on new things.</p>
+                <h2 className="text-left m-5">Team working</h2>
+                <p className="flex-auto px-4 text-center">Working with big teams it's important to me. I'm compromised to help and develop skills on new things.</p>
             </div>
             <div className="text-gray-50 bg-stone-500 flex items-center py-8 m-5">
-                <h2 className="text-left m-5">I'm a problem solver</h2>
+                <h2 className="text-left m-5">Solving problems</h2>
                 <p className="flex-auto px-4 text-center">I'm always looking for new ways to solve problems. I'm not afraid to take risks and try new things.</p>
             </div>
             <div className="text-gray-50 bg-stone-500 flex items-center py-8 m-5">
-                <h2 className="text-left m-5">I'm a team player</h2>
-                <p className="flex-auto px-4 text-center">I'm always looking for new ways to solve problems. I'm not afraid to take risks and try new things.</p>
+                <h2 className="text-left m-5">Deploying project</h2>
+                <p className="flex-auto px-4 text-center">Compromised to develop and deploy new technologies.</p>
             </div>
         </div>
     );
